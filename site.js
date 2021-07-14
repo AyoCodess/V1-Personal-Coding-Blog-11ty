4e5329c98285a8a36938f794db58230ad09da5ec
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-07-14 18:32:22 +0200

    test

---
+++ site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Ayo Codes - The Blog",
+  description:
+    "Follow the ups as well as the downs. This is my coding journey.",
+  rootUrl: "https://blog.ayoadesanya.com",
+  disqusShortname: "www-blog-ayoadesanya-com",
+};
